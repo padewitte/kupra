ccd90d73957e3419ad20923d8ae755d0d3c6e63d
Pierre-Alban <dewitte.pa@gmail.com>
2013-11-28 22:14:31 +0100

    Rewrite refactor
    Former-commit-id: 190bb107f12b43e4ebe1c239e6450115853cf746

--- src/main/java/org/mrc/restserver/launcher/MRCUrlRewrite.java
+++ src/main/java/org/mrc/restserver/launcher/MRCUrlRewrite.java
@@ -1,9 +1,12 @@
 package org.mrc.restserver.launcher;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.camel.Producer;
 import org.apache.camel.component.http.UrlRewrite;
@@ -16,30 +19,48 @@ import org.apache.camel.component.http.UrlRewrite;
  */
 public class MRCUrlRewrite implements UrlRewrite {
 
-	private Map<String, String> rewriteRules = new HashMap<String, String>();
-	private MRCServerBean contentServer;
+	private Map<Pattern, String> rewriteRules = new HashMap<Pattern, String>();
+	private Pattern contentPattern;
+	private String contentPort;
 
 	public MRCUrlRewrite(MRCServerBean contentServer,MRCServerBean documentationServer,
 			List<MRCServerBean> otherServers) {
-		rewriteRules.put(documentationServer.getDefaultContext(),
-				"" + documentationServer.getListenPort());
+		
+		Pattern documentationRule = Pattern.compile("(https?://[^/:]*)(?::\\d*)?/"+documentationServer.getDefaultContext()+"/?((?:[a-zA-Z-_\\/\\.0-9#:?=&;,]*)?)");
+		rewriteRules.put(documentationRule,
+				"" + documentationServer.getListenPort()+"/");
+		
 		for (MRCServerBean server : otherServers) {
-			rewriteRules.put(server.getDefaultContext(),
-					"" + server.getListenPort()+"/"+server.getDefaultContext());
+			rewriteRules.put(Pattern.compile("(https?://[^/:]*)(?::\\d*)?(/"+server.getDefaultContext()+"(?:[a-zA-Z-_\\/\\.0-9#:?=&;,]*)?)")
+			,
+					"" + server.getListenPort());
 		}
-		this.contentServer = contentServer;
+		this.contentPattern = Pattern.compile("(https?://[^/:]*)(?::\\d*)?(/?(?:[a-zA-Z-_\\/\\.0-9#:?=&;,]*)?)");
+		this.contentPort = ""+contentServer.getListenPort();
+		
 	}
 
 	public String rewrite(String url, String relativeUrl, Producer producer)
 			throws Exception {
 		String ret = null;
-		for (Entry<String, String> rule : rewriteRules.entrySet()) {
-			if (url.matches("http://.*" + (contentServer.getListenPort()) + "/"
-					+ rule.getKey() + ".*")) {
-				ret = url.replace(":" + (contentServer.getListenPort()) + "/"
-						+ rule.getKey(), ":" + rule.getValue());
-			} else if (url.contains("/" + rule.getKey())) {
-				ret = url.replace("/" + rule.getKey(), ":" + rule.getValue());
+		Iterator<Entry<Pattern, String>> ruleSet = rewriteRules.entrySet().iterator();
+
+		while (ruleSet.hasNext() && ret == null ) {
+			Entry<Pattern, String> rule = ruleSet.next();
+			Matcher matcher = rule.getKey().matcher(url);
+			if(matcher.find()){
+				ret = matcher.group(1) + ":"+rule.getValue();
+				if(matcher.group(2) != null){
+					ret = ret + matcher.group(2);
+				}
+			}
+		}
+		if(ret == null){
+			Matcher matcher = contentPattern.matcher(url);
+			if(matcher.find()){
+				ret = matcher.group(1) + ":"+contentPort+ matcher.group(2);
+			}else{
+				ret = url;
 			}
 		}
 		//URL  http://127.0.0.1:8670 not rewrite
